d193029951591261b8d219bd884f3b89830073c1
nguyenthanhbinh0411 <133386534+nguyenthanhbinh0411@users.noreply.github.com>
2024-09-18 04:00:38 +0700

    Upload Vue.js project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Movie/'  // Đường dẫn repo trên GitHub Pages
+    : '/'
+});
